78342d68df6e88465d6c683cb974b649be02fe6e
zedMar65 <marius.jedinkus@gmail.com>
2023-12-05 21:03:10 +0200

    eddited code

--- EncoderMain.py
+++ EncoderMain.py
@@ -1,4 +1,5 @@
-from mutagen.mp4 import MP4, MP4Cover
+from mutagen.mp4 import MP4
+from Core.FIleOct import convertToBin
 
 def write_metadata(input_bytes, output_file):
     # Create an MP4 instance
@@ -10,8 +11,17 @@ def write_metadata(input_bytes, output_file):
     # Save the MP4 file with the metadata
     mp4_file.save(output_file)
 
-# Example usage
-input_bytes = b'Your string of bytes here'
-output_file = 'output.mp4'
-
-write_metadata(input_bytes, output_file)
+if __name__ == "__main__":
+    try:
+        print("Input Path:")
+        file_path = input()
+        print("Mask Video input path:")
+        output_file = input()
+        
+        input_bytes = convertToBin(file_path)
+        if input_bytes is not None:
+            print("Processing...")
+            write_metadata(input_bytes, output_file)
+            print("Wrote to: ",output_file)
+    except KeyboardInterrupt:
+        print("Canceled")
