ab70888283943b293c92b90584e1ad7ddbd74f78
zedMar65 <marius.jedinkus@gmail.com>
2023-12-05 20:51:05 +0200

    Added Second branch

--- DecoderMain.py
+++ DecoderMain.py
@@ -1,15 +1,1 @@
-from Core.FIleOct import binaryToFile
-from Core.Video import vidToOct
-    
-if __name__ == "__main__":
-    try:
-        print("Input Video Path:")
-        input_video_path = input()
-        print("Processing...")
-        binStr = vidToOct(input_video_path)
-        binaryToFile(binStr)
-    except KeyboardInterrupt:
-        print("\nExecution interrupted by user.")
-
-    # except Exception as ex:
-    #     print(f"An unexpected error occurred: {ex}")
+#Fill
