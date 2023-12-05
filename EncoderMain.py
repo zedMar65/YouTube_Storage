c82fa7bd425c8b87b79e861abce186a167584416
zedMar65 <marius.jedinkus@gmail.com>
2023-12-05 20:59:29 +0200

    added some code

--- EncoderMain.py
+++ EncoderMain.py
@@ -1,1 +1,17 @@
-#Fill
+from mutagen.mp4 import MP4, MP4Cover
+
+def write_metadata(input_bytes, output_file):
+    # Create an MP4 instance
+    mp4_file = MP4()
+
+    # Set the metadata tag with the input bytes
+    mp4_file["\xa9day"] = [input_bytes]
+
+    # Save the MP4 file with the metadata
+    mp4_file.save(output_file)
+
+# Example usage
+input_bytes = b'Your string of bytes here'
+output_file = 'output.mp4'
+
+write_metadata(input_bytes, output_file)
